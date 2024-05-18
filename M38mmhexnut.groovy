9905be53e8667c4026cda7d43748049d2831ff51
technocopia <technocopia@technocopia-ThinkPad-T431s>
2024-05-18 02:03:14 -0400

    tried using a vitamin instead
    added vitamin

--- M38mmhexnut.groovy
+++ M38mmhexnut.groovy
@@ -1,1 +1,8 @@
 // code here
+import eu.mihosoft.vrl.v3d.Hexagon
+CSG hex = new Hexagon(	20, // Flat to flat radius
+                      		40 // Height
+                      		).toCSG()//convert to CSG to display                    			 
+                      		.movex(50)
+                      		.movey(50)
+CSG vitamin_nut_M3 = Vitamins.get("nut", "M3")
